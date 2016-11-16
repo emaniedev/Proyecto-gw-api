dd0efe8d2758790eb3e2bfde3712ac6c249395cb
masticapilas <maniega@outlook.es>
2016-11-17 00:04:04 +0100

    hh
    Primer commit FULL->PequeÃ±a interfaz y funcionando
    
    -Intento de creacion de Clase en JS.
    -Llamadas AJAX funcionando.
    -Comprendiendo los JSON devueltos.

---
+++ src/propio/personaje.js
@@ -0,0 +1,12 @@
+var Personaje = {
+    nombre : "",
+    raza : "",
+    profesion : "",
+    level : 0,
+    nuevoPersonaje : function (nom,raz,prof,lvl){
+        this.nombre = nom,
+        this.raza = raz,
+        this.profesion = prof,
+        this.level = lvl
+    }
+}
---
+++ src/propio/prueba.js
@@ -0,0 +1,106 @@
+/**
+ * Peticion de Cuenta
+ */
+/**
+ *      ------ MAIN ------
+ */
+    obtenerCuenta();
+    obtenerPersonajes();
+    volver();
+
+/**
+ *      ------ FIN MAIN ------
+ */
+
+/**
+ *      ------ FUNCIONES ------
+ */
+
+function obtenerCuenta (){
+    $("#buscar").click(function(){
+        $("#volver").show();
+        $("#busqueda").hide();
+        $("#listaDatos").show();
+            var key = $("#key").val();
+            $("#debug").html("p").text(key);
+            var urlApi = "https://api.guildwars2.com/v2/account?lang=es&access_token="+key;
+            $("#debug").html("p").text(urlApi);
+            $.getJSON(urlApi, function(data) {
+                var item = "";
+                $.each(data, function (key , val){
+                    var datos = key + ": " + val ;
+                    $("<li/>", { 
+                        "id": key,
+                        html : datos 
+                    }).appendTo("#listaDatos");
+                })
+            })
+
+            }
+    )}
+
+    function obtenerPersonajes (){
+        $("#buscar").click(function(){
+            var key = $("#key").val();
+            $("#debug").html("p").text(key);
+            var urlApi = "https://api.guildwars2.com/v2/characters?lang=es&access_token="+key;
+            $("#debug").html("p").text(urlApi);
+            $.getJSON(urlApi, function(data) {
+                var item = "";
+               
+                $.each(data, function (key , val){
+                    var datos = (key + 1) + ": " + val ;
+                     var click = "seleccionarPersonaje('"+val+"')";
+                    
+                    $("<button/>", { 
+                        "class" : "btn btn-default",
+                        "id": val,
+                        "value": val,
+                        html : val ,
+                        "onclick": click
+                    }).appendTo("#personajes");
+                })
+            })
+            
+        }
+    )}
+
+    function seleccionarPersonaje(keyp){
+        $("#listaDatos").hide();
+        $("#personaje").empty();
+            var key = $("#key").val();
+            var name = encodeURI(keyp);
+            var urlApi = "https://api.guildwars2.com/v2/characters/"+ name +"?lang=es&access_token="+key;
+            
+            $.getJSON(urlApi, function(data) {
+                var item = "";
+                var personaje = Personaje.nuevoPersonaje(data.name,data.race,data.profession,data.level);
+
+
+                $.each(personaje, function (key , val){
+                    var datos = (key) + ": " + val ;
+                    $("<li/>", { 
+                        "id": key,
+                        html : datos 
+                    }).appendTo("#personaje");
+                })
+            })
+
+        }
+
+    function volver (){
+        $("#volver").click(function(){
+            $("#volver").hide();
+            $("#busqueda").show();
+        })
+    }
+        
+    
+ 
+
+
+
+
+/**
+ *      ------ FIN FUNCIONES ------
+ */
